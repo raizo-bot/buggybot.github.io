86df63bd736e8e25748ed2b79ec5a10114e8d954
Satoqz <satoqzzz@gmail.com>
2020-03-30 20:06:22 +0200

    changed a lot lol

--- js/animationLoader.js
+++ js/animationLoader.js
@@ -3,7 +3,6 @@ var div3 = document.getElementById("div3");
 var div4 = document.getElementById("div4");
 var div5 = document.getElementById("div5");
 var div6 = document.getElementById("div6");
-var div7 = document.getElementById("div7");
 
 function isVisible (ele) {
     const { top, bottom } = ele.getBoundingClientRect();
@@ -16,11 +15,10 @@ function isVisible (ele) {
   }
 
 function addAnimation() {
-    if(isVisible(div2)) {div2.classList.add("fadeInRight")};
-    if(isVisible(div3)) {div3.classList.add("fadeInLeft")};
-    if(isVisible(div4)) {div4.classList.add("fadeInRight")};
-    if(isVisible(div5)) {div5.classList.add("fadeInLeft")};
-    if(isVisible(div6)) {div6.classList.add("fadeInRight")};
-    if(isVisible(div7)) {div7.classList.add("fadeInLeft")};
+    if(isVisible(div2)) {div2.classList.add("fadeInRightBig")};
+    if(isVisible(div3)) {div3.classList.add("fadeInLeftBig")};
+    if(isVisible(div4)) {div4.classList.add("fadeInRightBig")};
+    if(isVisible(div5)) {div5.classList.add("fadeInLeftBig")};
+    if(isVisible(div6)) {div6.classList.add("fadeInRightBig")};
 }
 
