cd09ea253d71525d64c69d5206a5f6edeb154f36
Satoqz <satoqzzz@gmail.com>
2020-03-30 17:11:51 +0200

    added INSANE ANIMATIONS

---
+++ js/animationLoader.js
@@ -0,0 +1,24 @@
+var div3 = document.getElementById("div3");
+var div4 = document.getElementById("div4");
+var div5 = document.getElementById("div5");
+var div6 = document.getElementById("div6");
+var div7 = document.getElementById("div7");
+
+function isVisible (ele) {
+    const { top, bottom } = ele.getBoundingClientRect();
+    const vHeight = (window.innerHeight || document.documentElement.clientHeight);
+  
+    return (
+      (top > 0 || bottom > 0) &&
+      top < vHeight
+    );
+  }
+
+function addAnimation() {
+    if(isVisible(div3)) {div3.classList.add("fadeInLeft")};
+    if(isVisible(div4)) {div4.classList.add("fadeInRight")};
+    if(isVisible(div5)) {div5.classList.add("fadeInLeft")};
+    if(isVisible(div6)) {div6.classList.add("fadeInRight")};
+    if(isVisible(div7)) {div7.classList.add("fadeInLeft")};
+}
+
