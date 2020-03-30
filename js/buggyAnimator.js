0137fe8a718988aea94d1a2af5ac48c4d20babba
Satoqz <satoqzzz@gmail.com>
2020-03-30 21:11:38 +0200

    very cool changes

--- js/buggyAnimator.js
+++ js/buggyAnimator.js
@@ -4,22 +4,22 @@ var c = document.getElementById("cursor");
 function writeBuggy() {
     setTimeout( () => b.innerHTML = "B", 0);
     setTimeout( () => b.innerHTML = "Bu", 400);
-    setTimeout( () => b.innerHTML = "Buu", 800);
+    setTimeout( () => b.innerHTML = "Buu", 1000);
     setTimeout( () => b.innerHTML = "Bu", 1600);
-    setTimeout( () => b.innerHTML = "Bug", 2200);
-    setTimeout( () => b.innerHTML = "Bugg", 2600);
-    setTimeout( () => b.innerHTML = "Buggg", 3000);
-    setTimeout( () => b.innerHTML = "Bugg", 3800);
-    setTimeout( () => b.innerHTML = "Buggy", 4400);
-    setTimeout( () => b.innerHTML = "Bugg", 7000);
-    setTimeout( () => b.innerHTML = "Bug", 7600);
-    setTimeout( () => b.innerHTML = "Bu", 8000);
-    setTimeout( () => b.innerHTML = "B", 8400);
-    setTimeout( () => writeBuggy(), 8400);
+    setTimeout( () => b.innerHTML = "Bug", 2500);
+    setTimeout( () => b.innerHTML = "Bugg", 3000);
+    setTimeout( () => b.innerHTML = "Buggg", 3600);
+    setTimeout( () => b.innerHTML = "Bugg", 4100);
+    setTimeout( () => b.innerHTML = "Buggy", 4700);
+    setTimeout( () => b.innerHTML = "Bugg", 11000);
+    setTimeout( () => b.innerHTML = "Bug", 11600);
+    setTimeout( () => b.innerHTML = "Bu", 12000);
+    setTimeout( () => b.innerHTML = "B", 12400);
+    setTimeout( () => writeBuggy(), 12400);
 };
 
 function cursor() {
-    setTimeout( () => c.innerHTML = "", 0);
+    setTimeout( () => c.innerHTML = "‎‎‎‎‎‎‏‏‎ ‎", 0);
     setTimeout( () => c.innerHTML = "|", 500);
     setTimeout( () => cursor(), 1000);
 }
